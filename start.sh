5b09c6e9c33f073467c4aaf42aeef430109db35b
Holden Karau <holden@pigscanfly.ca>
2015-12-16 13:45:43 -0800

    Add a header, also fix start script

--- start.sh
+++ start.sh
@@ -1,1 +1,1 @@
-IPYTHON_OPTS="notebook" ${SPARK_HOME}/spark/bin/pyspark --packages com.databricks:spark-csv_2.10:1.3.0
+IPYTHON_OPTS="notebook" ${SPARK_HOME}/bin/pyspark --packages com.databricks:spark-csv_2.10:1.3.0
