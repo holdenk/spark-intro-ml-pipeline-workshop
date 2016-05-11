91c8374647bf27d882b9101ba3a317a445b6cb92
Holden Karau <holden@us.ibm.com>
2016-05-10 19:59:08 -0700

    Avoid deprecated options in the start script, add a simple window example at the end, be awesome

--- start.sh
+++ start.sh
@@ -1,1 +1,1 @@
-IPYTHON_OPTS="notebook" ${SPARK_HOME}/bin/pyspark --packages com.databricks:spark-csv_2.10:1.3.0
+PYSPARK_DRIVER_PYTHON="ipython" PYSPARK_DRIVER_PYTHON_OPTS="notebook" ${SPARK_HOME}/bin/pyspark --packages com.databricks:spark-csv_2.10:1.3.0
---
+++ start_legacy.sh
@@ -0,0 +1,1 @@
+IPYTHON_OPTS="notebook" ${SPARK_HOME}/bin/pyspark --packages com.databricks:spark-csv_2.10:1.3.0
